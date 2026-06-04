37fb70ba361428506c7dcc44dfca3d09d6ea6bd4
None <None>
2026-06-04 08:42:23 -0700

    主机增加到6个

--- tests/complex_test/test_network.py
+++ tests/complex_test/test_network.py
@@ -38,26 +38,31 @@ class ComplexTopo(Topo):
         h3 = self.addHost('h3')
         h4 = self.addHost('h4')
         h5 = self.addHost('h5')
+        h6 = self.addHost('h6')
 
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
         s3 = self.addSwitch('s3')
         s4 = self.addSwitch('s4')
         s5 = self.addSwitch('s5')
+        s6 = self.addSwitch('s6')
 
         self.addLink(h1, s1)
         self.addLink(h2, s2)
         self.addLink(h3, s3)
         self.addLink(h4, s4)
         self.addLink(h5, s5)
+        self.addLink(h6, s6)
 
         self.addLink(s1, s2)
         self.addLink(s2, s3)
         self.addLink(s3, s4)
         self.addLink(s4, s5)
-        self.addLink(s5, s1)
+        self.addLink(s5, s6)
+        self.addLink(s6, s1)
         self.addLink(s1, s3)
         self.addLink(s2, s5)
+        self.addLink(s3, s6)
 
 
 def selected_ping_tests(net):
@@ -66,6 +71,8 @@ def selected_ping_tests(net):
         ('h2', 'h5'),
         ('h3', 'h1'),
         ('h4', 'h2'),
+        ('h5', 'h6'),
+        ('h6', 'h3'),
     ]
     failed = False
     for src_name, dst_name in tests:
